edff625dba5ff1cf76c31336cc3db7da63d7e18b
vineet-cyberkan <vineet1.st@gmail.com>
2019-05-17 15:16:43 +0530

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CircularComponent } from './circular/circular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CircularComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/circular/circular.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-circular',
+  templateUrl: './circular.component.html',
+  styleUrls: ['./circular.component.less']
+})
+export class CircularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
